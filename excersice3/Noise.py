9c1584841e9400b422055ae16c25aed4881ab3db
Xintong Hao <31745643+XintongHao@users.noreply.github.com>
2017-12-07 16:10:47 -0500

    Add files via upload

--- excersice3/Noise.py
+++ excersice3/Noise.py
@@ -1,87 +1,61 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
-import numpy as np
 import cv2
-import random
+import numpy as np
+import copy
 
-def Add_gaussian_Noise(srcArr,mean,sigma):
-    NoiseArr = srcArr.copy()
-    NoiseArr = np.random.normal(mean, sigma, NoiseArr.shape)
-    np.add(srcArr, NoiseArr, srcArr,casting="unsafe")
-    return
-    
-def Add_salt_pepper_Noise(srcArr, pa, pb):
-    rows,cols = srcArr.shape
-    amount1 = rows*cols*pa
-    amount2 = rows*cols*pb
-    for counter in range(int(amount1)):
-        srcArr[ int(random.uniform(0,rows)) ][ int(random.uniform(0,cols)) ] = 0
-    for counter in range(int(amount2)):
-        srcArr[ int(random.uniform(0,rows)) ][ int(random.uniform(0,cols)) ] = 225    
-        
+def Add_salt_pepper_Noise(image,pa,pb):
+    row,col,ch = image.shape
+    out = copy.deepcopy(image)
+    # Salt mode
+    num_salt = row*col*pb
+    coords = [np.random.randint(0, i - 1, int(num_salt))
+              for i in image.shape]
+    out[coords] = 1
+
+    # Pepper mode
+    num_pepper = row*col*pa
+    coords = [np.random.randint(0, i - 1, int(num_pepper))
+              for i in image.shape]
+    out[coords] = 0
+    return out
+
+def Add_gaussian_Noise(image,mean,sigma):
+    image_noise = copy.deepcopy(image)
+    im = np.zeros(image.shape, np.uint8)
+    cv2.randn(im,mean,sigma) # create the random distribution
+    image_noise=cv2.add(image_noise, im)
+    return image_noise
+
 def main():
-    image = cv2.imread('Test_images/baboon.jpg',0)
-    cv2.namedWindow('Original image')
-    cv2.imshow('Original image', image)
+    image = cv2.imread('Test_images/Lenna.png')
+    try:
+        image.shape
+        print("checked for shape".format(image.shape))
+    except AttributeError:
+        print("shape not found")
+    image_gau = Add_gaussian_Noise(image,0,50)
+    image_sp = Add_salt_pepper_Noise(image,0.01,0.01)
+    cv2.imshow("original image", image)
+    cv2.imshow("Add gaussian Noise", image_gau)
+    cv2.imshow("Add a&p Noise", image_sp)
+
+    image_box = cv2.blur(image_gau,(3,3))   
+    cv2.imshow("gua_noise Box filter", image_box)
     
-    #Gaussian Noise
-    noise_img = image.copy()
-    mean=20
-    sigma=50
-    Add_gaussian_Noise(noise_img, mean, sigma)
-    cv2.imshow('Gaussian Noise',noise_img)
-    cv2.imwrite('Gaussian Noise.jpg',noise_img)
+    image_gau_filter = cv2.GaussianBlur(image_gau,(3,3),1.5)
+    cv2.imshow("gua_noise Gaussian filter", image_gau_filter)
     
-    #Box filter
-    noise_dst = noise_img.copy()
-    cv2.blur(noise_dst,(7,7))
-    cv2.imshow('Box filter1',noise_dst)
-    cv2.imwrite('Box filter1.jpg',noise_dst)
-       
-    #Gaussian filter
-    noise_dst1 = noise_img.copy()
-    cv2.GaussianBlur(noise_dst1, (7,7), 1.5)
-    cv2.imshow('Gaussian filter1',noise_dst1)
-    cv2.imwrite('Gaussian filter1.jpg',noise_dst1)
+    iamge_median = cv2.medianBlur(image_gau,3)
+    cv2.imshow("gua_noise Median filter", iamge_median)
+      
+    image_box = cv2.blur(image_sp,(3,3))   
+    cv2.imshow("a&p Noise Box filter", image_box)
     
-    #Median filter
-    noise_dst2 = noise_img.copy()
-    cv2.medianBlur(noise_dst2,7)
-    cv2.imshow('Median filter1',noise_dst2)
-    cv2.imwrite('Median filter1.jpg',noise_dst2)  
+    image_gau_filter = cv2.GaussianBlur(image_sp,(3,3),1.5)
+    cv2.imshow("a&p Noise Gaussian filter", image_gau_filter)
     
-    #Salt and Pepper Noise
-    noise_img2 = image.copy()
-    pa=0.01
-    pb=0.01
-    Add_salt_pepper_Noise(noise_img2, pa, pb)
-    cv2.imshow('Salt and Pepper Noise',noise_img2)
-    cv2.imwrite('Salt and Pepper Noise.jpg',noise_img2) 
-    
-    #Box filter
-    noise_dst3 = noise_img2.copy()
-    cv2.blur(noise_dst3, (7,7))
-    cv2.imshow('Box filter2',noise_dst3)
-    cv2.imwrite('Box filter2.jpg',noise_dst3)
-    
-    #Gaussian filter
-    noise_dst4 = noise_img2.copy()
-    cv2.GaussianBlur(noise_dst4, (7,7), 1.5)
-    cv2.imshow('Gaussian filter2',noise_dst4)
-    cv2.imwrite('Gaussian filter2.jpg',noise_dst4)
-    
-    #Median filter
-    noise_dst5 = noise_img2.copy()
-    cv2.medianBlur(noise_dst5,7)
-    cv2.imshow('Median filter2',noise_dst5)
-    cv2.imwrite('Median filter2.jpg',noise_dst5)  
-    
-    
+    image_median = cv2.medianBlur(image_sp,3)    
+    cv2.imshow("a&p Noise Median filter", image_median)
     
     cv2.waitKey(0)
-    cv2.destroyAllWindows()
- 
-if __name__ == '__main__':
-    main()   
-
-                      
+    cv2.destroyAllWindows() ## Destroy all windows
+main()
